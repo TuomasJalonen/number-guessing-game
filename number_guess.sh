8d51e8d0343b8c81666d2e3d3fb1460820bac308
Tuomas Jalonen <tuomas.a.jalonen@gmail.com>
2026-04-15 13:38:32 -0400

    fix: test existence with user_id as user might exist without played games

--- number_guess.sh
+++ number_guess.sh
@@ -2,31 +2,36 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+# Random integer between 1-1000
 NUMBER=$(( RANDOM % 1000 + 1 ))
 
+# Get username as input
 echo "Enter your username:"
 read USERNAME
 
-# query number of games and smallest number of guesses
-DATA=$($PSQL "SELECT COUNT(game_id), MIN(guesses) FROM games RIGHT JOIN users ON games.user_id = users.user_id WHERE username = '$USERNAME'")
+# query user_id
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
 
-# no data -> new user
-if [[ -z $DATA ]]
+# no USER_ID -> new user
+if [[ ! $USER_ID ]]
 then
   # print welcome message
   echo Welcome, $USERNAME! It looks like this is your first time here.
   # add user to database (assume safe input)
   INSERT_OUT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
-  
-# data exists -> old user
+  # query user_id
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
+
+# USER_ID is not empty -> old user
 else
+  # query number of games and smallest number of guesses
+  DATA=$($PSQL "SELECT COUNT(game_id), MIN(guesses) FROM games WHERE user_id = '$USER_ID'")
   # split data into variables
   IFS='|' read -r GAMES_PLAYED BEST_GAME <<< $DATA
   # print welcome back message
   echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
+# ask user to input their guess
 echo "Guess the secret number between 1 and 1000:"
 read GUESS
-
-echo "
